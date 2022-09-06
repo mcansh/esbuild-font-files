c4ad2fe0bb696ff49c836e853e7b2595d6ec743a
Logan McAnsh <logan@mcan.sh>
2022-09-06 14:35:48 -0400

    copy css url imports
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- build.mjs
+++ build.mjs
@@ -49,32 +49,15 @@ const loaders = {
 let SRC_DIR = path.join(process.cwd(), "src");
 let OUT_DIR = path.join(process.cwd(), "public/build");
 
-async function build() {
-  console.log(kleur.dim("clearing build directory"));
-  await fse.emptyDir(OUT_DIR);
-
-  console.log(kleur.dim("finding source files"));
-  let sourceFiles = await fse.readdir(SRC_DIR);
-  let entryPoints = sourceFiles.map((file) => path.join(SRC_DIR, file));
-
-  console.log(kleur.cyan().bold("building..."));
+/**
+ *
+ * @param {import('esbuild').BuildOptions} config
+ */
+async function doTheBuild(config, name) {
+  console.log(kleur.cyan().bold(`[${name}] building...`));
 
   let start = Date.now();
-  let result = await esbuild.build({
-    entryPoints,
-    outdir: OUT_DIR,
-    splitting: true,
-    bundle: true,
-    format: "esm",
-    platform: "browser",
-    publicPath: "/build/",
-    loader: loaders,
-    plugins: [],
-    entryNames: "[dir]/[name]-[hash]",
-    chunkNames: "_shared/[name]-[hash]",
-    assetNames: "_assets/[name]-[hash]",
-    write: false,
-  });
+  let result = await esbuild.build({ ...config, write: false });
   let end = Date.now();
 
   if (result.errors.length > 0) {
@@ -92,7 +75,91 @@ async function build() {
 
   let duration = prettyMs(end - start);
 
-  console.log(kleur.cyan().bold(`built in ${duration}`));
+  console.log(kleur.cyan().bold(`[${name}] built in ${duration}`));
+
+  return result;
+}
+
+async function build() {
+  console.log(kleur.dim("clearing build directory"));
+  await fse.emptyDir(OUT_DIR);
+
+  console.log(kleur.dim("finding source files"));
+  let sourceFiles = await fse.readdir(SRC_DIR);
+  let entryPoints = sourceFiles.map((file) => path.join(SRC_DIR, file));
+
+  await doTheBuild(
+    {
+      entryPoints,
+      outdir: OUT_DIR,
+      splitting: true,
+      bundle: true,
+      format: "esm",
+      platform: "browser",
+      publicPath: "/build/",
+      loader: loaders,
+      plugins: [bundleCssPlugin()],
+      entryNames: "[dir]/[name]-[hash]",
+      chunkNames: "_shared/[name]-[hash]",
+      assetNames: "_assets/[name]-[hash]",
+    },
+    "main"
+  );
+}
+
+/** @returns {import('esbuild').Plugin} */
+function bundleCssPlugin() {
+  return {
+    name: "bundle-css",
+    async setup(build) {
+      let buildOptions = build.initialOptions;
+
+      build.onLoad({ filter: /\.css$/ }, async (args) => {
+        let { outfile, outdir, assetNames } = buildOptions;
+        let assetDirname = path.dirname(assetNames);
+        let result = await doTheBuild(
+          {
+            ...buildOptions,
+            minifySyntax: false,
+            incremental: false,
+            splitting: false,
+            sourcemap: false,
+            write: false,
+            outdir: path.join(
+              outfile ? path.dirname(outfile) : outdir,
+              assetDirname
+            ),
+            entryPoints: [args.path],
+            assetNames: "[name]-[hash]",
+            entryNames: "[dir]/[name]-[hash]",
+            publicPath: ".",
+            loader: {
+              ...buildOptions.loader,
+              ".css": "css",
+            },
+            metafile: true,
+            plugins: [],
+          },
+          "css"
+        );
+
+        let keys = Object.keys(result.metafile.outputs);
+        let entry = keys.find((key) => {
+          let { entryPoint } = result.metafile.outputs[key];
+          return !!entryPoint;
+        });
+
+        return {
+          contents: `export default "${path.join(
+            buildOptions.publicPath,
+            assetDirname,
+            path.basename(entry)
+          )}";`,
+          loader: "js",
+        };
+      });
+    },
+  };
 }
 
 build().then(
